0e8d36bcfd3ff7a38e41847ca68e9ba8be209790
MSI LeoPard <sweetdark@gmail.com>
2023-05-20 12:05:34 +0300

    add supermarket

--- lib/Constants/connection.dart
+++ lib/Constants/connection.dart
@@ -1,5 +1,5 @@
 class ConnectionUrls {
   static const String _deviceIp = "172.19.24.23";
-  static const String _localHost = "192.168.1.24";
+  static const String _localHost = "172.19.27.112";
   static String urlIp = "http://" + _localHost + ":8085/";
 }
