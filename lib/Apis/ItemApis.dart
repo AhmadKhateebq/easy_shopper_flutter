12baca18f2ce810c78310e32e6eb8bfdf1929d8a
MSI LeoPard <sweetdark@gmail.com>
2023-05-30 21:28:29 +0300

    يا قويطي كس امك يا ابن الكحبة

--- lib/Apis/ItemApis.dart
+++ lib/Apis/ItemApis.dart
@@ -24,4 +24,30 @@ class ItemApis {
       return http.Response("error", 404);
     }
   }
+
+  static Future<http.Response> createProduct(name,description,category,brand) async {
+    try {
+      var sp = await SharedPreferences.getInstance();
+      String? token =
+      sp.getString('userToken') != null ? sp.getString('userToken') : "";
+
+          return http.post(
+      Uri.parse(
+      "${ConnectionUrls.urlIp}/products/"),
+    headers: {
+    "content-type": "application/json",
+    "Authorization": "${header} ${token}",
+    },
+    body: jsonEncode(<String, dynamic>{
+    "name": name,
+    "description": description,
+    "category": category,
+    "brand": brand,
+    }));
+    } on Exception catch (e) {
+      print("getAllItems exception: $e");
+      return http.Response("error", 404);
+    }
+  }
+
 }
