52b5758a123f6c59aa0bb3e96727590660990a7b
MSI LeoPard <sweetdark@gmail.com>
2023-06-14 22:51:31 +0300

    fuck you

--- lib/Constants/connection.dart
+++ lib/Constants/connection.dart
@@ -1,5 +1,5 @@
 class ConnectionUrls {
-  static const String _deviceIp = "192.168.1.243";
-  static const String _localHost = "10.0.2.2";
+  static const String _deviceIp = "192.168.1.4";
+  static const String _localHost = "192.168.1.4";
   static String urlIp = "http://" + _localHost + ":8085/";
 }
