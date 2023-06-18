3b2ad4585a2c4147812a24a862797caa281cf91e
AhmadKhateebq <AhmadKhateebq@gmail.com>
2023-06-18 18:13:24 +0300

    s

---
+++ lib/Pages/customer/model/product_data.dart
@@ -0,0 +1,14 @@
+class Product {
+  String name;
+  String brand;
+  String category;
+  String description;
+  String imageUrl;
+  Product({
+    required this.name,
+    required this.brand,
+    required this.category,
+    required this.description,
+    required this.imageUrl,
+  });
+}
