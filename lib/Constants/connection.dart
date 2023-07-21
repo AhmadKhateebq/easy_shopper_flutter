1f5a74f7dc822c3fba1b142b9af72a7cab95c002
Mohammad <moh.teid@gmail.com>
2023-07-22 01:51:59 +0300

    change app color

--- lib/Constants/connection.dart
+++ lib/Constants/connection.dart
@@ -3,5 +3,5 @@
 class ConnectionUrls {
   static const String _deviceIp = "192.168.1.3";
   static const String _localHost = "10.0.2.2";
-  static String urlIp = "http://" + _deviceIp + ":8085/";
+  static String urlIp = "http://" + _localHost + ":8085/";
 }
