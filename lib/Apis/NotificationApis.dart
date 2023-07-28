568184697351da92be93591cfb754fb2eb227d9a
AhmadKhateebq <AhmadKhateebq@gmail.com>
2023-07-28 14:49:16 +0300

    token handle

--- lib/Apis/NotificationApis.dart
+++ lib/Apis/NotificationApis.dart
@@ -5,18 +5,35 @@ import 'package:http/http.dart';
 
 import '../Pages/customer/model/list_data.dart';
 
-class NotificationApis{
-  static Future<Response> handleList(int userId , List<SharedWith> canEditList
-  , List<SharedWith> newShared )async{
+class NotificationApis {
+  static Future<Response> handleList(int userId, List<SharedWith> canEditList,
+      List<SharedWith> newShared) async {
+    String body = jsonEncode(
+        {"userId": userId, "editShared": canEditList, "newShared": newShared});
 
+    var response = await Requests.postRequest("/notification/list", body);
+    print("notification list response : ${response.body}");
+    return response;
+  }
+
+  static Future<Response> addUser(int userId, String token) async {
     String body = jsonEncode({
-      "userId" : userId,
-      "editShared" : canEditList,
-      "newShared": newShared
+      "userId": userId,
+      "token": token,
     });
+    var response = await Requests.postRequest("/notification/add", body);
+    print("notification list response : ${response.body}");
+    return response;
+  }
 
-   var response =  await Requests.postRequest("/notification/list",body );
-   print("notification list response : ${response.body}");
+  static Future<Response> deleteUserTokin(int userId, String token) async {
+    String body = jsonEncode({
+      "userId": userId,
+      "token": token,
+    });
+    var response =
+        await Requests.postRequest("/notification/delete_user", body);
+    print("notification list response : ${response.body}");
     return response;
   }
 }
